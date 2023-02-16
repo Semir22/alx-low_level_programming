6305f767fe0d02f835e5788325517d93c2a57afa
Semir <semirmustafa22@gmail.com>
2023-02-16 11:50:40 -0800

    quote 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,12 @@
  * main - Entry point
  *
  * Return: Always 1 (Success)
- */
-int main(void)
-{
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+ */ 
  
-   	return (1); 
-}
+int write(int filedes, const char *buf, unsigned int nbyte); 
+int main( int argc, char** argv ) 
+{ 
+   write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
+ 
+   return (1); 
+} 
