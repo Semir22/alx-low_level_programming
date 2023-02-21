b0aa823f89d00766a0aa063d9452b1cc8a6e47d6
Semir <semirmustafa22@gmail.com>
2023-02-21 11:06:25 -0800

    First try

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void){ 
+	char *s="_putchar\n";
+	while (*s)
+		putchar(*s++);
+	return (0);
+}
